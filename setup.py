7ea071e5afd17c94e57657c9b870262a85b6dcab
videowala <51438045+videowala@users.noreply.github.com>
2020-06-18 13:12:36 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,19 @@ import uuid
 
 from setuptools import setup, find_packages
 
-try:  # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements
+# try:  # for pip >= 10
+#     from pip._internal.req import parse_requirements
+# except ImportError:  # for pip <= 9.0.3
+#     from pip.req import parse_requirements
 
-install_requirements = parse_requirements('requirements.txt', session=uuid.uuid1())
-requirements = [str(req.req) for req in install_requirements]
+# install_requirements = parse_requirements('requirements.txt', session=uuid.uuid1())
+# requirements = [str(req.req) for req in install_requirements]
+requirements = [
+    "oauthlib==2.0.1",
+    "requests==2.20.0",
+    "requests-oauthlib==0.7.0",
+    "six==1.10.0"
+]
 
 setup(
     name="python-unsplash",
